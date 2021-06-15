fc267cea81df833094e6fa11d4e5a85279119611
Amir Samir <amirative@gmail.com>
2021-06-15 23:07:11 +0800

    character service improvments

--- src/services/character.service.ts
+++ src/services/character.service.ts
@@ -2,7 +2,6 @@ import _ from 'lodash';
 import myAxios from '../config/axios'
 import myCache from '../config/cache'
 
-
 export async function getAllCharactersIds() {
 
     try {
@@ -10,71 +9,59 @@ export async function getAllCharactersIds() {
         let ids_array: any = [];
 
         //get the last modified record timestamp
-        const lastModifiedCharacter = await getLastModifiedCharacter();
-        let date = new Date(lastModifiedCharacter.modified);
+        const characters_api_details: any = await getCharachersDetails();
+
+        let last_modified_date = new Date(characters_api_details.last_modified);
         let last_modified_timestamp: any = myCache.get("last_modified_timestamp");
 
         //if last record timestamp equal to cached timestamp then serve cached data
-        if (last_modified_timestamp !== undefined && last_modified_timestamp == date.getTime()) {
+        if (last_modified_timestamp !== undefined && last_modified_timestamp == last_modified_date.getTime()) {
             ids_array = myCache.get("characters_id");
-            if (ids_array == undefined) {
-                ids_array = [];
-            }
-            else {
+            if (ids_array !== undefined) {
                 return ids_array;
             }
         }
 
-
-        //get all the records 
-        // paginate until no more records to grab
+        let promises: any = [];
         const limit: number = 100;
-        let page: number = 1;
         let stop: boolean = false;
 
-        do {
+        const number_of_pages = Math.ceil(characters_api_details.total / limit)
+
+        for (let page = 1; page <= number_of_pages; page++) {
             let params: any = {
                 limit: limit,
                 orderBy: 'modified',
                 offset: (page - 1) * limit
             }
 
-            // console.log(`Getting page ${page} with offset ${params.offset}`);
+            promises.push(getCharacters(params));
+        }
 
-            const result = await getCharacters(params);
+        let values = await Promise.all(promises);
 
+        for (let i = 0; i < values.length; i++) {
+            const result: any = values[i];
 
             if (result.code == 200 && result.data && result.data.results && result.data.results.length > 1) {
-
                 let ids = _.map(result.data.results, 'id');
-
                 ids_array = ids_array.concat(ids);
-
-                if (ids.length == limit) page++;
-                else stop = true;
-
-            }
-            else {
-                stop = true;
             }
         }
-        while (!stop);
-
 
         //cache results
         myCache.set("characters_id", ids_array);
-        myCache.set("last_modified_timestamp", new Date(lastModifiedCharacter.modified).getTime());
+        myCache.set("last_modified_timestamp", new Date(characters_api_details.last_modified).getTime());
 
         return ids_array;
 
     } catch (error) {
-        console.log(error);
-
+        // console.log(error);
         return null;
     }
 }
 
-export async function getLastModifiedCharacter() {
+export async function getCharachersDetails() {
     try {
         let params: any = {
             limit: 1,
@@ -82,10 +69,18 @@ export async function getLastModifiedCharacter() {
 
         }
 
+        let data = {
+            last_modified: null,
+            total: null
+        }
+
         const result = await getCharacters(params);
 
         if (result && result.code == 200 && result.data.results && Array.isArray(result.data.results) && result.data.results.length > 0) {
-            return result.data.results[0];
+            data.last_modified = result.data.results[0].modified;
+            data.total = result.data.total;
+
+            return data;
         }
 
         return null;
@@ -104,8 +99,8 @@ export function getCharacters(params: any) {
 
 export async function getCharacterById(id: string) {
 
+    //serve from cache if availabe
     const value = myCache.get("character-" + id);
-
 
     if (value !== undefined) {
         return value;
@@ -125,6 +120,7 @@ export async function getCharacterById(id: string) {
             description: data && data.description ? data.description : null
         }
 
+        //cache result
         myCache.set("character-" + id, character);
 
         return character;
